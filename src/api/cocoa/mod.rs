f3d781c66dc7727a588111d8111d0e7283f2eda1
Patrick Walton <pcwalton@mimiga.net>
2016-05-12 16:33:07 -0700

    cocoa: Reuse a single thread-local `NSEvent` instance instead of repeatedly creating a new one when waking up the event loop from another thread.
    This also avoids sending this event to the `NSApplication`, since that's
    needless overhead.
    
    Reduces CPU usage of a simple `requestAnimationFrame()` loop in Servo by
    23%.
    
    Partially addresses servo/servo#9844.

--- src/api/cocoa/mod.rs
+++ src/api/cocoa/mod.rs
@@ -204,11 +204,7 @@ impl WindowProxy {
     pub fn wakeup_event_loop(&self) {
         unsafe {
             let pool = NSAutoreleasePool::new(nil);
-            let event =
-                NSEvent::otherEventWithType_location_modifierFlags_timestamp_windowNumber_context_subtype_data1_data2_(
-                    nil, NSApplicationDefined, NSPoint::new(0.0, 0.0), NSEventModifierFlags::empty(),
-                    0.0, 0, nil, NSApplicationActivatedEventType, 0, 0);
-            NSApp().postEvent_atStart_(event, NO);
+            WAKEUP_EVENT.with(|wakeup_event| NSApp().postEvent_atStart_(*wakeup_event, NO));
             pool.drain();
         }
     }
@@ -860,7 +856,10 @@ unsafe fn NSEventToEvent(window: &Window, nsevent: id) -> Option<Event> {
     if nsevent == nil { return None; }
 
     let event_type = nsevent.eventType();
-    NSApp().sendEvent_(if let NSKeyDown = event_type { nil } else { nsevent });
+    match event_type {
+        NSKeyDown | NSApplicationDefined => {}
+        _ => NSApp().sendEvent_(nsevent),
+    }
 
     match event_type {
         NSLeftMouseDown         => { Some(MouseInput(Pressed, MouseButton::Left)) },
@@ -953,3 +952,25 @@ unsafe fn NSEventToEvent(window: &Window, nsevent: id) -> Option<Event> {
         _  => { None },
     }
 }
+
+thread_local! {
+    static WAKEUP_EVENT: id = {
+        unsafe {
+            let event =
+                NSEvent::otherEventWithType_location_modifierFlags_timestamp_windowNumber_context_subtype_data1_data2_(
+                    nil,
+                    NSApplicationDefined,
+                    NSPoint::new(0.0, 0.0),
+                    NSEventModifierFlags::empty(),
+                    0.0,
+                    0,
+                    nil,
+                    NSApplicationActivatedEventType,
+                    0,
+                    0);
+            msg_send![event, retain];
+            event
+        }
+    }
+}
+
